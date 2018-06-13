a6fe53774f90016fa018aa4bf6d2d63d3e9001e0
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 22:47:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 def oxford_comma(array)
-  new_array = array[array.size - 1]
+  new_array = []
+  new_array.push(array[array.size - 1])
   array.pop
   combined_string = array.join(", ")
   combined_string << ", and "
