c15c6f77ce7a05205dd03b1e11665e599e9646ca
Gabriel Valle <gvalle91@gmail.com>
2018-06-13 22:53:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,9 @@ def oxford_comma(array)
     combined_string = array[0]
   elsif array.size == 2
     array.pop
-    
+    combined_string = array[0]
+    combined_string << " and "
+    combined_string << new_array[0]
   else
     array.pop
     combined_string = array.join(", ")
